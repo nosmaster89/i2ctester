838948e87fff60672372113f1a08fcdd81793f29
nosmaster89 <nosmaster89@gmail.com>
2023-07-24 13:27:12 +0100

    added i2c address to the args

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ from adafruit_atecc.adafruit_atecc import ATECC, _WAKE_CLK_FREQ
 parser = argparse.ArgumentParser(description='Description of your script.')
 parser.add_argument('-i','--iterations', help='Number of iterations to run', required=False, default=100)
 parser.add_argument('-s','--slot', help='slot to use', required=False, default='0')
+parser.add_argument('-a','--i2c_address', help='i2c address to use', required=False, default='0x60')
 arg = parser.parse_args()
 
 slotId = int(arg.slot)
 it = int(arg.iterations)
 i2c = busio.I2C(board.SCL, board.SDA, frequency=_WAKE_CLK_FREQ)
 
-atecc = ATECC(i2c)
+atecc = ATECC(i2c,address=int(arg.i2c_address,16))
 data = b'\x01\x02\x03\x04\x05\x06\x07\x08\x09\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x30\x31\x32'
 results = []
 
